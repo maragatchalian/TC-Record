ca45a818bac71c5c2426e639a0c3985766727441
mara <maragatchalian@github.com>
2015-06-05 09:42:56 +0800

    Added a Navigation Bar

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -5,25 +5,38 @@
     <title>DietCake <?php readable_text($title) ?></title>
 
     <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/bootstrap/css/custom.css" rel="stylesheet">
     <style>
       body {
-        padding-top: 60px;
+        padding-top: 80px;
       }
     </style>
   </head>
 
   <body>
 
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="brand" href="#">DietCake Hello</a>
-        </div>
-      </div>
-    </div>
+  <div class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+  <div class="container">
+    <a class="brand" href="index">DietCake Hello</a>
+    <?php if(isset($_SESSION["username"])):?>
+    <p align="right">You are logged in as: <b><?php echo ($_SESSION["username"]); ?> </b> <br>
+    <a href="<?php readable_text(url('user/logout'));?>">Logout</a></p>
+    <?php endif?>
+  </div>
+  </div>
+  </div>
 
     <div class="container">
 
+  <ul class="nav">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Trainees</a></li>
+    <li><a href="#">Courses</a></li>
+    <li><a href="#">Exam Scores</a></li>
+  </ul>
+
+ 
       <?php echo $_content_ ?>
 
     </div>
