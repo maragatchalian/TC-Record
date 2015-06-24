2ed66b7b21c847cfbdaf2399fe1cc50af6777e4d
mara <maragatchalian@github.com>
2015-06-24 15:36:14 +0800

    Modified gitignore and CSS files

--- app/views/trainee/index.php
+++ app/views/trainee/index.php
@@ -1,4 +1,5 @@
 <h3> Trainees </h3>
+<link href="/bootstrap/css/custom.css" rel="stylesheet">
 
 <a class="btn btn-medium btn-info" href="<?php readable_text(url('trainee/add_trainee')) ?>">Add Trainee</a>
 <br />
@@ -71,104 +72,7 @@ Display Trainees by:
 </div>
 
 
-<style type="text/css">
-body {
-    background:#00000 url('body-bg.jpg');
-}
- 
-.clearfix:after {
-    display:block;
-    clear:both;
-}
- 
-/*----- Menu Outline -----*/
-.menu-wrap {
-    width:100%;
-    box-shadow:0px 1px 3px rgba(0,0,0,0.2);
-    background:#3e3436;
-}
- 
-.menu {
-    width:1000px;
-    margin:0px auto;
-}
- 
-.menu li {
-    margin:0px;
-    list-style:none;
-    font-family:'Ek Mukta';
-}
- 
-.menu a {
-    transition:all linear 0.15s;
-    color:#919191;
-}
- 
-.menu li:hover > a, .menu .current-item > a {
-    text-decoration:none;
-    color:#be5b70;
-}
- 
-.menu .arrow {
-    font-size:11px;
-    line-height:0%;
-}
- 
-/*----- Top Level -----*/
-.menu > ul > li {
-    float:left;
-    display:inline-block;
-    position:relative;
-    font-size:14px;
-}
- 
-.menu > ul > li > a {
-    padding:10px 40px;
-    display:inline-block;
-    text-shadow:0px 1px 0px rgba(0,0,0,0.4);
-}
- 
-.menu > ul > li:hover > a, .menu > ul > .current-item > a {
-    background:#2e2728;
-}
- 
-/*----- Bottom Level -----*/
-.menu li:hover .sub-menu {
-    z-index:1;
-    opacity:1;
-}
- 
-.sub-menu {
-    width:160%;
-    padding:5px 0px;
-    position:absolute;
-    top:100%;
-    left:0px;
-    z-index:-1;
-    opacity:0;
-    transition:opacity linear 0.15s;
-    box-shadow:0px 2px 3px rgba(0,0,0,0.2);
-    background:#2e2728;
-}
- 
-.sub-menu li {
-    display:block;
-    font-size:14px;
-}
- 
-.sub-menu li a {
-    padding:10px 30px;
-    display:block;
-}
- 
-.sub-menu li a:hover, .sub-menu .current-item a {
-    background:#3e3436;
-}
-</style>
-
-
-
-<!-- Table goes in the document BODY -->
+<!-- Index Table -->
 <table class="gridtable">
 <tr>
     <th> Employee ID </th>
@@ -181,34 +85,6 @@ body {
     <th> Current Course </th>
     <th> Exam Date </th>
 </tr>
-
-
-<!--Trainee Table-->
-<style type="text/css">
-table.gridtable {
-    font-family: verdana,arial,sans-serif;
-    font-size:14px;
-    color:#333333;
-    border-width: 1px;
-    border-color: #666666;
-    border-collapse: collapse;
-}
-table.gridtable th {
-    border-width: 1px;
-    padding: 8px;
-    border-style: solid;
-    border-color: #666666;
-    background-color: #dedede;
-}
-table.gridtable td {
-    border-width: 1px;
-    padding: 8px;
-    border-style: solid;
-    border-color: #666666;
-    background-color: #ffffff;
-}
-</style>
-
 
 <?php foreach ($trainees as $get_from_trainee): ?>
     <tr>
