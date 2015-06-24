fc2d84140222bb4d4b2a1863249b90537c956424
mara <maragatchalian@github.com>
2015-06-24 15:20:29 +0800

    Modified links on Trainee Index

--- app/views/trainee/index.php
+++ app/views/trainee/index.php
@@ -3,13 +3,13 @@
 <a class="btn btn-medium btn-info" href="<?php readable_text(url('trainee/add_trainee')) ?>">Add Trainee</a>
 <br />
 <br />
-Sort Trainee by:
+Display Trainees by:
 <br />
-<br />
+
 <div class="menu-wrap">
     <nav class="menu">
         <ul class="clearfix">
-                     
+
         <!--Sort by Date Hired-->
             <li>
                 <a href="<?php readable_text(url('trainee/index')) ?>">Date Hired</span></a>
@@ -66,11 +66,6 @@ Sort Trainee by:
                     </li>   
                 </ul>
             </li>    
-
-
-
-
-
         </ul>
     </nav>
 </div>
@@ -172,6 +167,22 @@ body {
 </style>
 
 
+
+<!-- Table goes in the document BODY -->
+<table class="gridtable">
+<tr>
+    <th> Employee ID </th>
+    <th> Date Hired</th>
+    <th> Last Name </th>
+    <th> First Name </th>
+    <th> Batch </th>
+    <th> Skill </th>
+    <th> Training Status </th>
+    <th> Current Course </th>
+    <th> Exam Date </th>
+</tr>
+
+
 <!--Trainee Table-->
 <style type="text/css">
 table.gridtable {
@@ -197,19 +208,6 @@ table.gridtable td {
     background-color: #ffffff;
 }
 </style>
-<!-- Table goes in the document BODY -->
-<table class="gridtable">
-<tr>
-    <th> Employee ID </th>
-    <th> Date Hired</th>
-    <th> Last Name </th>
-    <th> First Name </th>
-    <th> Batch </th>
-    <th> Skill </th>
-    <th> Training Status </th>
-    <th> Current Course </th>
-    <th> Exam Date </th>
-</tr>
 
 
 <?php foreach ($trainees as $get_from_trainee): ?>
@@ -217,8 +215,7 @@ table.gridtable td {
         <td><a href="<?php readable_text(url('trainee/view_trainee_profile', array('trainee_id' => $get_from_trainee->id))) ?>">
         <?php readable_text($get_from_trainee->employee_id) ?> </a> </td>
 
-        <td><a href="<?php readable_text(url('#', array('trainee_id' => $get_from_trainee->id))) ?>">
-        <?php readable_text($get_from_trainee->hired) ?> </a> </td>
+        <td> <?php readable_text($get_from_trainee->hired) ?> </a> </td>
 
         <td><a href="<?php readable_text(url('trainee/view_trainee_profile', array('trainee_id' => $get_from_trainee->id))) ?>">
         <?php readable_text($get_from_trainee->last_name) ?> </a> </td>
@@ -226,17 +223,13 @@ table.gridtable td {
         <td><a href="<?php readable_text(url('trainee/view_trainee_profile', array('trainee_id' => $get_from_trainee->id))) ?>">
         <?php readable_text($get_from_trainee->first_name) ?> </a> </td>
     
-        <td><a href="<?php readable_text(url('trainee/view_trainee_profile', array('trainee_id' => $get_from_trainee->id))) ?>">
-        <?php readable_text($get_from_trainee->batch) ?> </a> </td>
+        <td> <?php readable_text($get_from_trainee->batch) ?> </a> </td>
 
-        <td><a href="<?php readable_text(url('trainee/view_trainee_profile', array('trainee_id' => $get_from_trainee->id))) ?>">
-        <?php readable_text($get_from_trainee->skill_set) ?> </a> </td>
+        <td> <?php readable_text($get_from_trainee->skill_set) ?> </a> </td>
 
-        <td><a href="<?php readable_text(url('trainee/view_trainee_profile', array('trainee_id' => $get_from_trainee->id))) ?>">
-        <?php readable_text($get_from_trainee->training_status) ?> </a> </td>
+        <td> <?php readable_text($get_from_trainee->training_status) ?> </a> </td>
 
-        <td><a href="<?php readable_text(url('#', array('trainee_id' => $get_from_trainee->id))) ?>">
-        <?php readable_text($get_from_trainee->course_status) ?> </a> </td>
+        <td> <?php readable_text($get_from_trainee->course_status) ?> </a> </td>
 
     <?php endforeach; ?> 
     </tr>
