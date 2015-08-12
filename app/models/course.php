2b2ec8fd8e00d9a237a93e8901b6a347ad732743
mara <maragatchalian@github.com>
2015-08-12 11:38:54 +0800

    Changed getCategory to getCategories

--- app/models/course.php
+++ app/models/course.php
@@ -63,7 +63,7 @@ class Course extends AppModel
         $db->commit();
     }
 
-    public static function getCategory()
+    public static function getCategories()
     {
         $db = DB::conn();
         $rows = $db->rows("SELECT DISTINCT category FROM course order by category");
